492a6e95b5ec1b657a9ad59e0ea2b2fef048d49d
Ignatov Egor <ign-e-d@proton.me>
2024-02-21 01:27:52 +0300

    Minor changes

--- Common-modules/systemd.nix
+++ Common-modules/systemd.nix
@@ -2,12 +2,12 @@
 {
   systemd.user.services.NixOS-check-updates = {
     enable = true;
-    path = [pkgs.bash pkgs.git pkgs.coreutils pkgs.libsForQt5.kdialog pkgs.polkit pkgs.systemd pkgs.libsForQt5.konsole];
+    path = [pkgs.bash pkgs.git pkgs.coreutils pkgs.libsForQt5.kdialog pkgs.polkit pkgs.systemd pkgs.libsForQt5.konsole pkgs.sudo];
     wantedBy = [ "graphical-session.target" ];
     serviceConfig = {
       Type = "oneshot";
       RemainAfterExit = "yes";
-      ExecStartPre = "${pkgs.coreutils}/bin/sleep 20";
+      ExecStartPre = "${pkgs.coreutils}/bin/sleep 10";
       ExecStart = "${pkgs.bash}/bin/bash /home/a/Documents/Share/NixOS-modules/check-updates.sh";
     };
     environment = {
