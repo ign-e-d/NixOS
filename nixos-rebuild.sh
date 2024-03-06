81a8433de9283d863ea125c8574dfa3ef505eded
Ignatov Egor <ign-e-d@proton.me>
2024-03-06 22:57:16 +0300

    441

--- nixos-rebuild.sh
+++ nixos-rebuild.sh
@@ -3,9 +3,19 @@
 
 figlet "#$HOSTNAME"
 
-echo "sudo nixos-rebuild switch --flake .#$HOSTNAME"
+cat << EOF
 
-sudo nixos-rebuild switch --flake .#$HOSTNAME &>logs/nixos-rebuild.log || (cat nixos-switch.log | grep --color error && false)
+This simple script will do several things:
+1. Execute "sudo nixos-rebuild switch --flake .#$HOSTNAME";
+2. Reenable NixOS-check-updates.service;
+3. Restart NixOS-check-updates.service;
+4. Commit changes in configuration and push to repository.
+
+EOF
+
+echo '1. Execute "sudo nixos-rebuild switch --flake .#$HOSTNAME"'
+
+sudo nixos-rebuild switch --flake .#$HOSTNAME &>logs/nixos-rebuild.log || (cat nixos-rebuild.log | grep --color error && false)
 
 if [ $? -eq 0 ]; then
   kdialog --title "rebuild.sh" --msgbox "nixos-rebuild switch\nExecuted successfully"
@@ -17,17 +27,23 @@ else
   exit 0
 fi
 
-systemctl --user reenable NixOS-check-updates.service
+echo "2. Reenable NixOS-check-updates.service"
+
+systemctl --user reenable NixOS-check-updates.service &>logs/NixOS-check-updates-reenable.log
 if [ $? -eq 1 ]; then
   kdialog --title "rebuild.sh" --error "systemctl --user reenable NixOS-check-updates.service\nFail"
   exit 1
 fi
 
-systemctl --user restart NixOS-check-updates.service
+echo "3. Restart NixOS-check-updates.service"
+
+systemctl --user restart NixOS-check-updates.service &>logs/NixOS-check-updates-restart.log
 if [ $? -eq 1 ]; then
   kdialog --title "rebuild.sh" --error "systemctl --user restart NixOS-check-updates.service\nFail"
   exit 1
 fi
+
+echo "4. Commit changes in configuration and push to repository"
 
 git fetch
 
