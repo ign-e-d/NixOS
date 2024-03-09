e48a2e5e2f3531547f235e0403a3b73714e918e6
Ignatov Egor <ign-e-d@proton.me>
2024-03-10 00:23:58 +0300

    B550-541

--- system/non-host-specific/services.nix
+++ system/non-host-specific/services.nix
@@ -6,7 +6,6 @@
 {
   services.xserver.enable = true;
   services.xserver.displayManager.sddm.enable = true;
-  #services.xserver.desktopManager.plasma5.enable = true;
   services.xserver.desktopManager.plasma6.enable = true;
   services.xserver = {
     xkb.layout = "us";
