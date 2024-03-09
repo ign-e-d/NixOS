44e22a01e2749fa7ea656878519d13231eb6d4da
Ignatov Egor <ign-e-d@proton.me>
2024-03-10 00:28:20 +0300

    B550-542

--- system/host-specific/B550/environment.nix
+++ system/host-specific/B550/environment.nix
@@ -4,24 +4,5 @@
 
 { config, pkgs, ... }:
 {
-  environment.sessionVariables = {
-    QT_QPA_PLATFORM = "wayland";
-    QT_QPA_PLATFORMTHEME = "qt6ct";
-  };
 
-  environment.systemPackages = with pkgs; [
-    kdePackages.qt6ct
-    kdePackages.breeze
-    qt6.qtwayland
-  ];
-
-  # fix
-  nixpkgs.config.qt6 = {
-    enable = true;
-    platformTheme = "qt6ct";
-    style = {
-      package = pkgs.catppuccin-kvantum;
-      name = "kvantum";
-    };
-  };
 }
