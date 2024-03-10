c9d7dc782db5d5def48941e87c3575dd0ec526d6
Ignatov Egor <ign-e-d@proton.me>
2024-03-10 19:03:24 +0300

    B550-572

--- home-manager/host-specific/B550/home.nix
+++ home-manager/host-specific/B550/home.nix
@@ -11,34 +11,12 @@
     qt5.qtwayland
     libsForQt5.qtstyleplugin-kvantum
     libsForQt5.lightly
-    catppuccin-kvantum
+    #catppuccin-kvantum
+    (catppuccin-kvantum.override {
+      accent = "Peach";
+      variant = "Frappe";
+    })
   ];
-
-  #home.pointerCursor = {
-    #gtk.enable = true;
-    # x11.enable = true;
-    #package = pkgs.bibata-cursors;
-    #name = "Bibata-Modern-Classic";
-    #size = 16;
-  #};
-
-  #gtk = {
-  #  enable = true;
-  #  theme = {
-  #    package = pkgs.flat-remix-gtk;
-  #    name = "Flat-Remix-GTK-Grey-Darkest";
-  #  };
-
-  #  iconTheme = {
-  #    package = pkgs.gnome.adwaita-icon-theme;
-  #    name = "Adwaita";
-  #  };
-
-  #  font = {
-  #    name = "Sans";
-  #    size = 11;
-  #  };
-  #};
 
   qt = {
     enable = true;
@@ -49,17 +27,13 @@
     };
   };
 
-  xdg.configFile = {
-    "Kvantum/Catppuccin-Frappe-Blue/Catppuccin-Frappe-Blue/Catppuccin-Frappe-Blue.kvconfig".source = "${pkgs.catppuccin-kvantum}/share/Kvantum/Catppuccin-Frappe-Blue/Catppuccin-Frappe-Blue.kvconfig";
-    "Kvantum/Catppuccin-Frappe-Blue/Catppuccin-Frappe-Blue/Catppuccin-Frappe-Blue.svg".source = "${pkgs.catppuccin-kvantum}/share/Kvantum/Catppuccin-Frappe-Blue/Catppuccin-Frappe-Blue.svg";
-  };
+  #xdg.configFile = {
+  #  "Kvantum/Catppuccin-Frappe-Blue/Catppuccin-Frappe-Blue/Catppuccin-Frappe-Blue.kvconfig".source = "${pkgs.catppuccin-kvantum}/share/Kvantum/Catppuccin-Frappe-Blue/Catppuccin-Frappe-Blue.kvconfig";
+  #  "Kvantum/Catppuccin-Frappe-Blue/Catppuccin-Frappe-Blue/Catppuccin-Frappe-Blue.svg".source = "${pkgs.catppuccin-kvantum}/share/Kvantum/Catppuccin-Frappe-Blue/Catppuccin-Frappe-Blue.svg";
+  #};
 
-#  qt = {
-#    enable = true;
-#    platformTheme = "kde";
-#    style = {
-#      package = pkgs.libsForQt5.breeze-qt5;
-#      name = "breeze";
-#    };
-#  };
+  xdg.configFile = {
+    "Kvantum/Catppuccin-Frappe-Peach/Catppuccin-Frappe-Peach/Catppuccin-Frappe-Peach.kvconfig".source = "${pkgs.catppuccin-kvantum}/share/Kvantum/Catppuccin-Frappe-Peach/Cattpuccin-Frappe-Peach.kvconfig";
+    "Kvantum/Catppuccin-Frappe-Peach/Catppuccin-Frappe-Peach/Catppuccin-Frappe-Peach.svg".source = "${pkgs.catppuccin-kvantum}/share/Kvantum/Catppuccin-Frappe-Peach/Cattpuccin-Frappe-Peach.svg";
+  };
 }
