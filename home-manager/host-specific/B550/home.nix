c1e40ff74af25d1ca003a9cdf422faac3b7021f3
Ignatov Egor <ign-e-d@proton.me>
2024-03-10 15:15:28 +0300

    B550-553

--- home-manager/host-specific/B550/home.nix
+++ home-manager/host-specific/B550/home.nix
@@ -7,5 +7,15 @@
   home.packages = with pkgs; [
     blender
     swww
+
+    utterly-nord-plasma
   ];
+
+      # https://discourse.nixos.org/t/guide-to-installing-qt-theme/35523/2
+    xdg.configFile = {
+      "Kvantum/Utterly-Nord-Solid-Plasma/Utterly-Nord-Solid/Utterly-Nord-Solid.kvconfig".source = "${pkgs.utterly-nord-plasma}/share/Kvantum/Utterly-Nord-Solid/Utterly-Nord-Solid.kvconfig";
+      "Kvantum/Utterly-Nord-Solid-Plasma/Utterly-Nord-Solid/Utterly-Nord-Solid.svg".source = "${pkgs.utterly-nord-plasma}/share/Kvantum/Utterly-Nord-Solid/Utterly-Nord-Solid.svg";
+      "Kvantum/Utterly-Nord-Solid-Plasma/Utterly-Nord-Solid/Nord.patchconfig".source = "${pkgs.utterly-nord-plasma}/share/Kvantum/Utterly-Nord-Solid/Nord.patchconfig";
+      "Kvantum/kvantum.kvconfig".text = "[General]\ntheme=Utterly-Nord-Solid";
+    };
 }
