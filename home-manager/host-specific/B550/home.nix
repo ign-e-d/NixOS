aa5f2c5301041017bebe5e3349fb936e9b290730
Ignatov Egor <ign-e-d@proton.me>
2024-03-10 19:57:33 +0300

    B550-578

--- home-manager/host-specific/B550/home.nix
+++ home-manager/host-specific/B550/home.nix
@@ -3,6 +3,10 @@
 #
 
 { config, pkgs, inputs, ... }:
+let
+  catppuccin-accent = "Peach";
+  catppuccin-variant = "Latte";
+in
 {
   home.packages = with pkgs; [
     blender
@@ -11,11 +15,11 @@
     qt5.qtwayland
     libsForQt5.qtstyleplugin-kvantum
     libsForQt5.lightly
-    #catppuccin-kvantum
     (catppuccin-kvantum.override {
-      accent = "Peach";
-      variant = "Frappe";
+      accent = catppuccin-accent;
+      variant = catppuccin-variant;
     })
+    #nordic
   ];
 
   qt = {
@@ -27,13 +31,14 @@
     };
   };
 
+  xdg.configFile = {
+    "Kvantum/Catppuccin-${catppuccin-variant}-${catppuccin-accent}/Catppuccin-${catppuccin-variant}-${catppuccin-accent}/Catppuccin-${catppuccin-variant}-${catppuccin-accent}.kvconfig".source = "${pkgs.catppuccin-kvantum}/share/Kvantum/Catppuccin-${catppuccin-variant}-${catppuccin-accent}/Cattpuccin-${catppuccin-variant}-${catppuccin-accent}.kvconfig";
+    "Kvantum/Catppuccin-${catppuccin-variant}-${catppuccin-accent}/Catppuccin-${catppuccin-variant}-${catppuccin-accent}/Catppuccin-${catppuccin-variant}-${catppuccin-accent}.svg".source = "${pkgs.catppuccin-kvantum}/share/Kvantum/Catppuccin-${catppuccin-variant}-${catppuccin-accent}/Cattpuccin-${catppuccin-variant}-${catppuccin-accent}.svg";
+  };
+
   #xdg.configFile = {
-  #  "Kvantum/Catppuccin-Frappe-Blue/Catppuccin-Frappe-Blue/Catppuccin-Frappe-Blue.kvconfig".source = "${pkgs.catppuccin-kvantum}/share/Kvantum/Catppuccin-Frappe-Blue/Catppuccin-Frappe-Blue.kvconfig";
-  #  "Kvantum/Catppuccin-Frappe-Blue/Catppuccin-Frappe-Blue/Catppuccin-Frappe-Blue.svg".source = "${pkgs.catppuccin-kvantum}/share/Kvantum/Catppuccin-Frappe-Blue/Catppuccin-Frappe-Blue.svg";
+  #  "Kvantum/Nordic/Nordic.kvconfig".source = "${pkgs.catppuccin-kvantum}/share/Kvantum/Nordic/Nordic.kvconfig";
+  #  "Kvantum/Nordic/Nordic.svg".source = "${pkgs.catppuccin-kvantum}/share/Kvantum/Nordic/Nordic.svg";
   #};
 
-  xdg.configFile = {
-    "Kvantum/Catppuccin-Frappe-Peach/Catppuccin-Frappe-Peach/Catppuccin-Frappe-Peach.kvconfig".source = "${pkgs.catppuccin-kvantum}/share/Kvantum/Catppuccin-Frappe-Peach/Cattpuccin-Frappe-Peach.kvconfig";
-    "Kvantum/Catppuccin-Frappe-Peach/Catppuccin-Frappe-Peach/Catppuccin-Frappe-Peach.svg".source = "${pkgs.catppuccin-kvantum}/share/Kvantum/Catppuccin-Frappe-Peach/Cattpuccin-Frappe-Peach.svg";
-  };
 }
