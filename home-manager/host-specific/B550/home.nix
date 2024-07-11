cabd568acc0f06be3b32c7dbdaea39a93110d7c0
Ignatov Egor <ign-e-d@proton.me>
2024-07-11 20:08:27 +0300

    B550-689

--- home-manager/host-specific/B550/home.nix
+++ home-manager/host-specific/B550/home.nix
@@ -3,38 +3,8 @@
 #
 
 { config, pkgs, inputs, ... }:
-let
-  catppuccin-accent = "Peach";
-  catppuccin-variant = "Latte";
-in
 {
   home.packages = with pkgs; [
-    # swww  Wallpapers for wayland
-    # hyprshot
-    # dunst
-    # libnotify
-    # networkmanager
 
-    # qt5.qtwayland
   ];
-
-  # home.pointerCursor = {
-  #   gtk.enable = true;
-  #   package = pkgs.bibata-cursors;
-  #   name = "Bibata-Modern-Classic";
-  #   size = 16;
-  # };
-
-  # qt = {
-  #   enable = true;
-  #   platformTheme = "qtct";
-  # };
-
-  # gtk = {
-  #   enable = true;
-  #   theme = {
-  #     package = pkgs.adw-gtk3;
-  #     name = "adw-gtk3";
-  #   };
-  # };
 }
