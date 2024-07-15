d7fded342d09860cf11ae62830d8fdd1f3de3217
Ignatov Egor <ign-e-d@proton.me>
2024-07-15 16:01:41 +0300

    B550-714

--- system/host-specific/B550/stylix.nix
+++ system/host-specific/B550/stylix.nix
@@ -4,11 +4,11 @@
 
 { config, pkgs, ... }:
 {
-    stylix.enable = true;
+    #stylix.enable = true;
 
 #    stylix.polarity = "dark";
 
     #stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/gruvbox-dark-hard.yaml";
 
-    stylix.image = /home/a/NixOS/pictures/wallpaper.jpg;
+    #stylix.image = /home/a/NixOS/pictures/wallpaper.jpg;
 }
