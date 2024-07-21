1eee5e3db232b8e91b16f86af0e15f5fb5bbff77
Ignatov Egor <ign-e-d@proton.me>
2024-07-21 21:50:14 +0300

    B550-745

--- home-manager/non-host-specific/programs/stylix.nix
+++ home-manager/non-host-specific/programs/stylix.nix
@@ -5,6 +5,7 @@
 { config, pkgs, inputs, ... }:
 {
   stylix.targets.gtk.enable = false;
+  stylix.targets.kde.enable = false;
   stylix.targets.kitty.enable = false;
   stylix.targets.helix.enable = false;
 }
