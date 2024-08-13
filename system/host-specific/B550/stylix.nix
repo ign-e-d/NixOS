c748ea671f49f5a91034f540431b68186b184105
Ignatov Egor <ign-e-d@proton.me>
2024-08-13 22:02:10 +0300

    T430s-86

--- system/host-specific/B550/stylix.nix
+++ system/host-specific/B550/stylix.nix
@@ -4,18 +4,11 @@
 
 { config, pkgs, ... }:
 {
-    stylix.enable = true;
+    # stylix.enable = true;
 
-    stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/gruvbox-dark-hard.yaml";
+    # stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/gruvbox-dark-hard.yaml";
 
-    # stylix.fonts = {
-    #   monospace = {
-    #     package = pkgs.nerdfonts.override {fonts = [ "DroidSansMono" ]; };
-    #     name = "DroidSansMNerdFontMono";
-    #   };
-    # };
+    # stylix.targets.grub.useImage = true;
 
-    stylix.targets.grub.useImage = true;
-
-    stylix.image = /home/a/NixOS/pictures/wallpaper2.jpg;
+    # stylix.image = /home/a/NixOS/pictures/wallpaper2.jpg;
 }
---
+++ system/non-host-specific/stylix.nix
@@ -0,0 +1,14 @@
+#
+# This file is intended for non-host-specific settings.
+#
+
+{ config, pkgs, ... }:
+{
+    stylix.enable = true;
+
+    stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/gruvbox-dark-hard.yaml";
+
+    stylix.targets.grub.useImage = true;
+
+    stylix.image = /home/a/NixOS/pictures/wallpaper2.jpg;
+}
