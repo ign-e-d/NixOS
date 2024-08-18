1c071cee8dea7a6c207f9c84f911f5469d6b137e
Ignatov Egor <ign-e-d@proton.me>
2024-08-18 11:11:37 +0300

    B550-757

--- system/non-host-specific/stylix.nix
+++ system/non-host-specific/stylix.nix
@@ -8,6 +8,10 @@
 
     stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/gruvbox-dark-hard.yaml";
 
+    stylix.cursor.package = pkgs.vanilla-dmz;
+    stylix.cursor.name = "DMZ (White)";
+    stylix.cursor.size = 24;
+
     stylix.targets.grub.useImage = true;
 
     stylix.image = /home/a/NixOS/pictures/wallpaper2.jpg;
