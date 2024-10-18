efd52269040de0cf91f134833a96df35ff069ae9
Ignatov Egor <ign-e-d@proton.me>
2024-10-18 14:18:06 +0300

    B550-787

--- scripts/nixos-rebuild.sh
+++ scripts/nixos-rebuild.sh
@@ -8,36 +8,25 @@ config_folder=$(dirname "$(dirname "$0")")
 
 figlet "#$HOSTNAME"
 
-echo "-----------------------------------------------------------"
-echo "This simple script will do several things:"
-echo "0. Execute sudo nix flake update $config_folder/"
-echo "1. Execute sudo nixos-generate-config --show-hardware-config > $config_folder/system/host-specific/$HOSTNAME/hardware-configuration.nix"
-echo "2. Execute sudo nixos-rebuild switch --flake $config_folder#$HOSTNAME"
-echo "3. Commit changes in configuration and push to repository"
-echo "-----------------------------------------------------------"
-
-# Add all files for the correct rebuilding
 git -C $config_folder add -A
 git -C $config_folder status
 
 echo ""
-read -r -p "0. Do you want to execute sudo nix flake update $config_folder/ ? [y/N] " response
+read -r -p "sudo nix flake update $config_folder/ ? [y/N] " response
 if [[ "$response" =~ ^([yY][eE][sS]|[yY])$ ]]
 then
   sudo nix flake update $config_folder/
 fi
 
 echo ""
-echo "1. Execute sudo nixos-generate-config --show-hardware-config > $config_folder/system/host-specific/$HOSTNAME/hardware-configuration.nix"
+echo "sudo nixos-generate-config --show-hardware-config > $config_folder/system/host-specific/$HOSTNAME/hardware-configuration.nix"
 sudo nixos-generate-config --show-hardware-config > $config_folder/system/host-specific/$HOSTNAME/hardware-configuration.nix
 
 echo ""
-echo "2. Execute sudo nixos-rebuild switch --flake $config_folder#$HOSTNAME"
-# sudo nixos-rebuild --verbose switch --flake $config_folder#$HOSTNAME
+echo "nh os switch $config_folder -- --impure"
 nh os switch $config_folder -- --impure # todo fix and remove impure
 
 echo ""
-echo "3. Commit changes in configuration and push to repository"
 read -r -p "Do you want to commit changes and push to repository? [y/N] " response
 if [[ "$response" =~ ^([yY][eE][sS]|[yY])$ ]]
 then
