60e36fad7190ca68f294b0b9da94a6d9a16c82d7
Ignatov Egor <ign-e-d@proton.me>
2025-01-11 21:44:48 +0300

    B550-829

--- home-manager/non-host-specific/programs/bash.nix
+++ home-manager/non-host-specific/programs/bash.nix
@@ -14,7 +14,7 @@
     enableCompletion = true;
     bashrcExtra = ''
       # Set frame rate
-      xranrd --output HDMI-A-0 --rate 75
+      xrandr --output HDMI-A-0 --rate 75
 
       # Change keyboard layout
       setxkbmap -option grp:ctrl_shift_toggle us,ru
