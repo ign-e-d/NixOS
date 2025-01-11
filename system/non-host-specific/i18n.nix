085a86ad44452f7fcd9ab94a335e85908a0e73c9
Ignatov Egor <ign-e-d@proton.me>
2025-01-11 15:31:24 +0300

    B550-809

--- system/non-host-specific/i18n.nix
+++ system/non-host-specific/i18n.nix
@@ -4,17 +4,17 @@
 
 { config, pkgs, ... }:
 {
-  i18n.defaultLocale = "en_US.UTF-8";
+  i18n.defaultLocale = "en_GB.UTF-8";
 
-  i18n.extraLocaleSettings = {
-    LC_ADDRESS = "ru_RU.UTF-8";
-    LC_IDENTIFICATION = "ru_RU.UTF-8";
-    LC_MEASUREMENT = "ru_RU.UTF-8";
-    LC_MONETARY = "ru_RU.UTF-8";
-    LC_NAME = "ru_RU.UTF-8";
-    LC_NUMERIC = "ru_RU.UTF-8";
-    LC_PAPER = "ru_RU.UTF-8";
-    LC_TELEPHONE = "ru_RU.UTF-8";
-    LC_TIME = "ru_RU.UTF-8";
-  };
+  # i18n.extraLocaleSettings = {
+  #   LC_ADDRESS = "ru_RU.UTF-8";
+  #   LC_IDENTIFICATION = "ru_RU.UTF-8";
+  #   LC_MEASUREMENT = "ru_RU.UTF-8";
+  #   LC_MONETARY = "ru_RU.UTF-8";
+  #   LC_NAME = "ru_RU.UTF-8";
+  #   LC_NUMERIC = "ru_RU.UTF-8";
+  #   LC_PAPER = "ru_RU.UTF-8";
+  #   LC_TELEPHONE = "ru_RU.UTF-8";
+  #   LC_TIME = "ru_RU.UTF-8";
+  # };
 }
