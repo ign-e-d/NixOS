c26a0033441679d7671563202dfb86609e45a21b
Ignatov Egor <ign-e-d@proton.me>
2025-01-11 19:54:59 +0300

    B550-825

--- dwm-6.5/config.def.h
+++ dwm-6.5/config.def.h
@@ -1,5 +1,10 @@
 /* See LICENSE file for copyright and license details. */
 
+/* patches */
+/*
+	1. https://dwm.suckless.org/patches/movestack/
+*/
+
 /* appearance */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
@@ -62,6 +67,7 @@ static const char *termcmd[]  = { "kitty", NULL };
 static const char *rebuildcmd[] = { "kitty", "--hold", "bash", "/home/a/NixOS/scripts/error-wrap.sh", NULL };
 static const char *v2rayAcmd[] = { "kitty", "sudo", "v2rayA", NULL };
 
+#include "movestack.c"
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
@@ -73,6 +79,8 @@ static const Key keys[] = {
 	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
 	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
 	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
+	{ MODKEY|ShiftMask,             XK_j,      movestack,      {.i = +1 } },
+	{ MODKEY|ShiftMask,             XK_k,      movestack,      {.i = -1 } },
 	{ MODKEY,                       XK_Return, zoom,           {0} },
 	{ MODKEY,                       XK_Tab,    view,           {0} },
 	{ MODKEY|ShiftMask,             XK_c,      killclient,     {0} },
--- system/host-specific/B550/services.nix
+++ system/host-specific/B550/services.nix
@@ -8,13 +8,17 @@
 
   services.xserver.windowManager.dwm.enable = true;
 
-  services.xserver.windowManager.dwm.package = pkgs.dwm.override {
-    conf = ../../../dwm-6.5/config.def.h;
-    # patches = [
-    #   (pkgs.fetchpatch {
-    #     url = "https://dwm.suckless.org/patches/movestack/dwm-movestack-6.1.diff";
-    #     hash = "sha256-PUd+P1WF4Ac5azB6tpYjbPo6XTrsqw/G9Y3mjvhHjmM=";
-    #   })
-    # ];
+  # services.xserver.windowManager.dwm.package = pkgs.dwm.override {
+  #   conf = ../../../dwm-6.5/config.def.h;
+  #   patches = [
+  #     (pkgs.fetchpatch {
+  #       url = "https://dwm.suckless.org/patches/movestack/dwm-movestack-6.1.diff";
+  #       hash = "sha256-PUd+P1WF4Ac5azB6tpYjbPo6XTrsqw/G9Y3mjvhHjmM=";
+  #     })
+  #   ];
+  # };
+
+  services.xserver.windowManager.dwm.package = pkgs.dwm.overrideAttrs {
+    src = ../../../dwm-6.5;
   };
 }
