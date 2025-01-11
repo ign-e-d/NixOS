d558c852f522d7aed637a3acbdee5626aa7c5fc2
Ignatov Egor <ign-e-d@proton.me>
2025-01-11 18:29:26 +0300

    B550-816

--- system/host-specific/B550/services.nix
+++ system/host-specific/B550/services.nix
@@ -6,6 +6,8 @@
 {
   services.xserver.videoDrivers = [ "amdgpu" ];
 
+  services.xserver.windowManager.dwm.enable = true;
+
   # services.xserver.windowManager.dwm.package = pkgs.dwm.override {
   #   conf = ../../../dwm-6.5/config.def.h;
   # };
