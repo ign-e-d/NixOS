2322dc4e9874cc3172dd134d954e1a8fc8b760b3
Ignatov Egor <ign-e-d@proton.me>
2025-01-11 19:14:47 +0300

    B550-821

--- dwm-6.5/config.def.h
+++ dwm-6.5/config.def.h
@@ -59,7 +59,7 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "kitty", NULL };
-static const char *rebuild[] = { "bash", "~/NixOS/scripts/kitty-wrap.sh", NULL };
+static const char *rebuildcmd[] = { "kitty", "bash", "~/NixOS/scripts/error-wrap.sh", NULL };
 
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
@@ -96,7 +96,7 @@ static const Key keys[] = {
 	TAGKEYS(                        XK_8,                      7)
 	TAGKEYS(                        XK_9,                      8)
 	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
-	{ MODKEY,                       XK_r,      spawn,          {.v = rebuild } },
+	{ MODKEY,                       XK_r,      spawn,          {.v = rebuildcmd } },
 };
 
 /* button definitions */
