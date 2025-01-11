3d5ca4932a2c220c7e91ffd3b792778f45184291
Ignatov Egor <ign-e-d@proton.me>
2025-01-11 11:36:26 +0300

    B550-803

--- system/non-host-specific/fonts.nix
+++ system/non-host-specific/fonts.nix
@@ -5,6 +5,6 @@
 { config, pkgs, ... }:
 {
   fonts.packages = with pkgs; [
-    (nerdfonts.override { fonts = [ "DroidSansMono" ]; })
+    nerd-fonts.droid-sans-mono
   ];
 }
--- system/non-host-specific/hardware.nix
+++ system/non-host-specific/hardware.nix
@@ -9,10 +9,10 @@
   hardware.graphics.enable = true;
   # hardware.opengl.driSupport = true; no longer has any effect
   hardware.graphics.enable32Bit = true;
-  hardware.graphics.extraPackages = with pkgs; [
-    rocm-opencl-icd
-    rocm-opencl-runtime
-  ];
+  # hardware.graphics.extraPackages = with pkgs; [ 11.01.25 doesnt exist anymore?
+  #   rocm-opencl-icd
+  #   rocm-opencl-runtime
+  # ];
 
   hardware.steam-hardware.enable = true;
 }
