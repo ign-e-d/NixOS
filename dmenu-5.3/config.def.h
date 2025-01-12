251f4da7fcf6637a3c21ba93e56cfeeb15a2dfc3
Ignatov Egor <ign-e-d@proton.me>
2025-01-12 18:11:23 +0300

    B550-847

--- dmenu-5.3/config.def.h
+++ dmenu-5.3/config.def.h
@@ -1,13 +1,22 @@
 /* See LICENSE file for copyright and license details. */
 /* Default settings; can be overriden by command line. */
 
-static int topbar = 0;                      /* -b  option; if 0, dmenu appears at bottom     */
+/* patches */
+/*
+	1. https://tools.suckless.org/dmenu/patches/center/dmenu-center-20240616-36c3d68.diff
+*/
+
+static int topbar = 0;                           /* -b  option; if 0, dmenu appears at bottom     */
+static int centered = 1;                         /* -c option; centers dmenu on screen */
+static int min_width = 500;                      /* minimum width when centered */
+static const float menu_height_ratio = 4.0f;     /* This is the ratio used in the original calculation */
+
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
 	"DroidSansMNerdFontMono:size=12",
 	"monospace:size=14"
 };
-static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
+static const char *prompt           = NULL;      /* -p  option; prompt to the left of input field */
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
