6ff5abc45a6e73c179328ada6298ca9181c7c230
Ignatov Egor <ign-e-d@proton.me>
2025-01-12 18:05:15 +0300

    B550-846

--- dmenu-5.3/config.def.h
+++ dmenu-5.3/config.def.h
@@ -1,17 +1,25 @@
 /* See LICENSE file for copyright and license details. */
 /* Default settings; can be overriden by command line. */
 
-static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
+static int topbar = 0;                      /* -b  option; if 0, dmenu appears at bottom     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"monospace:size=10"
+	"DroidSansMNerdFontMono:size=12",
+	"monospace:size=14"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
+static const char col_gray1[]       = "#222222";
+static const char col_gray2[]       = "#444444";
+static const char col_gray3[]       = "#bbbbbb";
+static const char col_gray4[]       = "#eeeeee";
+static const char col_cyan[]        = "#005577";
+static const char col_yellow[]      = "#d79921";
+static const char col_orange[]      = "#d65d0e";
 static const char *colors[SchemeLast][2] = {
-	/*     fg         bg       */
-	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#eeeeee", "#005577" },
-	[SchemeOut] = { "#000000", "#00ffff" },
+	/*               fg         bg       */
+	[SchemeNorm] = { col_gray3, col_gray1  },
+	[SchemeSel]  = { col_gray4, col_orange },
+	[SchemeOut]  = { "#000000", "#00ffff"  },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
