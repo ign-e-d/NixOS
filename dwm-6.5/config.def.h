0a19f6ce521d654e10d8950decfc7dab0735a2b1
Ignatov Egor <ign-e-d@proton.me>
2025-01-19 17:32:05 +0300

    B550-884

--- dwm-6.5/config.def.h
+++ dwm-6.5/config.def.h
@@ -12,6 +12,11 @@
 /* appearance */
 static const unsigned int borderpx  = 3;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
+static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
+static const unsigned int systrayonleft = 0;    /* 0: systray in the right corner, >0: systray on left of status text */
+static const unsigned int systrayspacing = 2;   /* systray spacing */
+static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
+static const int showsystray        = 1;        /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = {
@@ -35,7 +40,6 @@ static const char *colors[][3]      = {
 static const char *const autostart[] = {
 	"slstatus", NULL,
 	"xrandr", "--output", "HDMI-A-0", "--mode", "1920x1080", "--rate", "75", NULL,
-	// "setxkbmap", "-option", "grp:alt_shift_toggle", "us,ru", NULL,
 	"hsetroot", "-fill", "/home/a/NixOS/pictures/ARR02.png", NULL,
 	NULL /* terminate */
 };
